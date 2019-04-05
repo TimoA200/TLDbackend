1fe29b7d93f046de5c5eb427bd8a1a2f3b8fea10
Timo Aeckerle <timoaeckerle@gmx.de>
2019-04-05 21:03:41 +0200

    test

--- src/main/java/core/Match.java
+++ src/main/java/core/Match.java
@@ -1,6 +1,7 @@
 package core;
 
 import libs.Exec;
+import libs.Logger;
 
 public class Match extends Thread {
 
@@ -12,6 +13,7 @@ public class Match extends Thread {
     public void run() {
         super.run();
         Exec.cmd("/home/mastermind/csgo-multiserver/csgo-server @hey create");
+        Logger.log("test");
         Exec.cmd("rpl '${GSLT-\"\"}' '${GSLT-\"8A3477957A706E4C923FFDF0C757265E\"}' /root/csgo@hey/msm.d/cfg/server.conf");
     }
 }
