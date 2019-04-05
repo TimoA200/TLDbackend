165ec92575287c2f046a5e741156ec69bb75903e
Timo Aeckerle <timoaeckerle@gmx.de>
2019-04-05 22:06:05 +0200

    test

--- match.sh
+++ match.sh
@@ -2,5 +2,5 @@
 echo 'hey whats up?'
 
 /home/mastermind/csgo-multiserver/csgo-server @$1 create
-sed -i 's/${GSLT-""}'/'${GSLT-"$2"}/g' /root/csgo@$1/msm.d/cfg/server.conf
+sed -i 's/${GSLT-""}'/'${GSLT-"${2}"}/g' /root/csgo@$1/msm.d/cfg/server.conf
 echo 'wow'
