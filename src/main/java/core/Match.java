3582ec87243ab4d995f35767bb9054f99a1f1ac1
Timo Aeckerle <timoaeckerle@gmx.de>
2019-04-05 21:23:16 +0200

    (*/ω＼*)

--- src/main/java/core/Match.java
+++ src/main/java/core/Match.java
@@ -14,7 +14,7 @@ public class Match extends Thread {
         super.run();
         Exec.cmd("/home/mastermind/csgo-multiserver/csgo-server @hey create");
         //Exec.cmd("rpl '${GSLT-\"\"}' '${GSLT-\"8A3477957A706E4C923FFDF0C757265E\"}' /root/csgo@hey/msm.d/cfg/server.conf");
-        Exec.cmd("sed -i 's/${GSLT-\"\"}/${GSLT-\"8A3477957A706E4C923FFDF0C757265E\"}' /root/csgo@hey/msm.d/cfg/server.conf");
+        Exec.cmd("sed -i 's/${GSLT-\"\"}/${GSLT-\"8A3477957A706E4C923FFDF0C757265E\"/d}' /root/csgo@hey/msm.d/cfg/server.conf");
         //Exec.cmd("ls");
     }
 }
