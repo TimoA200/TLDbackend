985b6504458d696c51c332d9ab036bdf7acc52a5
Timo Aeckerle <timoaeckerle@gmx.de>
2019-04-05 22:20:33 +0200

    Add start command for server

--- match.sh
+++ match.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-echo 'hey whats up?'
-
 /home/mastermind/csgo-multiserver/csgo-server @$1 create
 sed -i 's/${GSLT-""}'/'${GSLT-"'"$2"'"}/g' /root/csgo@$1/msm.d/cfg/server.conf
-echo 'wow'
+/home/mastermind/csgo-multiserver/csgo-server @$1 start
