e6d30ff276529753b382e69f665681e903282c2a
Timo Aeckerle <timoaeckerle@gmx.de>
2019-04-05 20:59:39 +0200

    Get output of exec

--- src/main/java/libs/Exec.java
+++ src/main/java/libs/Exec.java
@@ -1,8 +1,11 @@
 package libs;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Exec {
+
 
     public static void cmd(String command) {
         cmd(command, true);
@@ -12,6 +15,15 @@ public class Exec {
         try {
             Process proc = Runtime.getRuntime().exec(command);
             proc.waitFor();
+            BufferedReader buf = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            String line = "";
+            String output = "";
+
+            while((line = buf.readLine()) != null) {
+                output += line + "\n";
+            }
+
+            Logger.log(output);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
