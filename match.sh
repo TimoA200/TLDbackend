decc4ea0f77d37d1a2e4adaf3d82df1a672f0bab
Timo Aeckerle <timoaeckerle@gmx.de>
2019-04-06 11:33:06 +0200

    test

--- match.sh
+++ match.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 /home/mastermind/csgo-multiserver/csgo-server @$1 create
 sed -i 's/${GSLT-""}'/'${GSLT-"'"$2"'"}/g' /root/csgo@$1/msm.d/cfg/server.conf
-/home/mastermind/csgo-multiserver/csgo-server @$1 start
-whoami
+#/home/mastermind/csgo-multiserver/csgo-server @$1 start
