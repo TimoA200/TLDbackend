83e66edbb9f3223fd581295ed296ba68bc043ee2
Timo Aeckerle <timoaeckerle@gmx.de>
2019-04-06 11:34:39 +0200

    sleep 15000

--- src/main/java/core/Match.java
+++ src/main/java/core/Match.java
@@ -15,7 +15,7 @@ public class Match extends Thread {
         Exec.cmd("TLDbackend/match.sh yeet 8A3477957A706E4C923FFDF0C757265E");
         try {
             Logger.log("sleep 5000");
-            sleep(5000);
+            sleep(15000);
             Exec.cmd("/home/mastermind/csgo-multiserver/csgo-server @yeet start");
         } catch (InterruptedException e) {
             e.printStackTrace();
