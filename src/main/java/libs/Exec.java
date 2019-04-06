de36f1993ed805a967888097d7817df00ae96eb2
Timo Aeckerle <timoaeckerle@gmx.de>
2019-04-06 11:04:43 +0200

    test

--- src/main/java/libs/Exec.java
+++ src/main/java/libs/Exec.java
@@ -14,19 +14,17 @@ public class Exec {
     public static void cmd(String command, boolean wait) {
         try {
             Process proc = Runtime.getRuntime().exec(command);
+            proc.waitFor();
+            BufferedReader buf = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            String line = "";
+            String output = "";
 
-            if(wait) {
-                proc.waitFor();
-                BufferedReader buf = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-                String line = "";
-                String output = "";
-
-                while ((line = buf.readLine()) != null) {
-                    output += line + "\n";
-                }
-
-                Logger.log(output);
+            while((line = buf.readLine()) != null) {
+                output += line + "\n";
             }
+
+            Logger.log(output);
+
             proc.destroy();
         } catch (IOException e) {
             e.printStackTrace();
