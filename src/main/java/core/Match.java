4ab0c54ee6dcfd2b1dc34f13368faf4c0834c9cf
Timo Aeckerle <timoaeckerle@gmx.de>
2019-04-06 10:51:44 +0200

    test

--- src/main/java/core/Match.java
+++ src/main/java/core/Match.java
@@ -12,6 +12,6 @@ public class Match extends Thread {
     @Override
     public void run() {
         super.run();
-        Exec.cmd("TLDbackend/match.sh yeet 8A3477957A706E4C923FFDF0C757265E");
+        Exec.cmd("TLDbackend/match.sh yeet 8A3477957A706E4C923FFDF0C757265E", false);
     }
 }
--- src/main/java/libs/Exec.java
+++ src/main/java/libs/Exec.java
@@ -14,17 +14,19 @@ public class Exec {
     public static void cmd(String command, boolean wait) {
         try {
             Process proc = Runtime.getRuntime().exec(command);
-            proc.waitFor();
-            BufferedReader buf = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-            String line = "";
-            String output = "";
 
-            while((line = buf.readLine()) != null) {
-                output += line + "\n";
+            if(wait) {
+                proc.waitFor();
+                BufferedReader buf = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+                String line = "";
+                String output = "";
+
+                while ((line = buf.readLine()) != null) {
+                    output += line + "\n";
+                }
+
+                Logger.log(output);
             }
-
-            Logger.log(output);
-
             proc.destroy();
         } catch (IOException e) {
             e.printStackTrace();
