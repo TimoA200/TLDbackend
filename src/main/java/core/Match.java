c418204d7b22149e7537ed4892275c34da0d1023
Timo Aeckerle <timoaeckerle@gmx.de>
2019-04-11 23:16:50 +0200

    lslslsl

--- src/main/java/core/Match.java
+++ src/main/java/core/Match.java
@@ -14,6 +14,9 @@ public class Match extends Thread {
         super.run();
         Exec.cmd("TLDbackend/match.sh yeet 8A3477957A706E4C923FFDF0C757265E");
         Exec.cmd("/home/mastermind/csgo-multiserver/csgo-server @yeet start");
+        Exec.cmd("/home/mastermind/csgo-multiserver/csgo-server @yeet start");
+        Exec.cmd("/home/mastermind/csgo-multiserver/csgo-server @yeet start");
+        Exec.cmd("/home/mastermind/csgo-multiserver/csgo-server @yeet start");
         Exec.cmd("echo 'aäsiogjiäüasufhgiawerihf'");
         Exec.cmd("echo 'aäsiogjiäüasufhgiawerihf'");
         Exec.cmd("echo 'aäsiogjiäüasufhgiawerihf'");
