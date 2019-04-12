691c05d8fc65e17155b8756b739b7b547d352649
Timo Aeckerle <timoaeckerle@gmx.de>
2019-04-12 15:42:28 +0200

    dd

--- src/main/java/core/Match.java
+++ src/main/java/core/Match.java
@@ -13,7 +13,6 @@ public class Match extends Thread {
     public void run() {
         super.run();
         Exec.cmd("TLDbackend/match.sh yeet 8A3477957A706E4C923FFDF0C757265E");
-        Exec.cmd("shutdown now -r");
         Exec.cmd("echo 'aäsiogjiäüasufhgiawerihf'");
     }
 }
