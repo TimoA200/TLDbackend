e01beb32a1f766473f1df030f9263f0a736c74b8
Timo Aeckerle <timoaeckerle@gmx.de>
2019-04-13 17:00:52 +0200

    fix path

--- backend.py
+++ backend.py
@@ -11,7 +11,7 @@ class Backend(WebSocket):
             client.sendMessage(self.address[0] + u' - ' + self.data)
         print('message received from: ' + self.address[0] + " -> " + self.data)
         if self.data == 'create match':
-            os.system('TLDbackend/match.sh yeet 8A3477957A706E4C923FFDF0C757265E')
+            os.system('./match.sh yeet 8A3477957A706E4C923FFDF0C757265E')
 
     def handleConnected(self):
         print(self.address, 'connected')
