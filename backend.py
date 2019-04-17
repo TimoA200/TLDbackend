58d4baa8aa5c7802bc1b5b2ebd53a888f6d8ce48
Timo Aeckerle <timoaeckerle@gmx.de>
2019-04-17 16:48:44 +0200

    test

--- backend.py
+++ backend.py
@@ -16,8 +16,8 @@ def creategslt():
     os.system(command)
     html = open("gslt.html").read()
     soup = BeautifulSoup(html)
-    gslt = soup.find_all("td", string="test")[0].find_previous_sibling("td").find_previous_sibling("td")
-    print('gslt -> ' + str(gslt))
+    gslt = soup.find_all("td", string="test")[0].find_previous_sibling("td").find_previous_sibling("td").decode_contents()
+    print('gslt -> ' + gslt)
 
 
 def deletegslt():
