1eb9aa24ba601c88d85838bb140abf5190424925
Timo Aeckerle <timoaeckerle@gmx.de>
2019-04-17 18:09:38 +0200

    final test for gslt

--- backend.py
+++ backend.py
@@ -18,7 +18,7 @@ def creategslt():
     soup = BeautifulSoup(html, 'html.parser')
     gslt = soup.find("td", string="test").find_previous_sibling("td").find_previous_sibling("td").decode_contents()
     print('gslt -> ' + gslt)
-    print('successfully created gslt')
+    print('successfully created gslt for server with id -> test')
 
 
 def deletegslt():
@@ -26,12 +26,16 @@ def deletegslt():
     html = open("gslt/test.html").read()
     soup = BeautifulSoup(html, 'html.parser')
     steamid = soup.find("td", string="test").find_next_sibling("td").find("input", {"name":"steamid"})['value']
-    print('steamid -> ' + steamid)
-    #steamid = soup.find_all("td", string="test")[0].find_next_sibling("td").find_all
+    command = 'curl -d "steamid=85568392922903941&sessionid=7f6485556d7104ef41146653" --header "Cookie: sessionid=7f6485556d7104ef41146653; steamCountry=DE%7C1014e5d9cdff2826218d409fb1eb5a56; timezoneOffset=7200,0; _ga=GA1.2.275275590.1555411498; steamLoginSecure=76561198832942031%7C%7CD71E1EE78B42609277E0314337FD4546AD467576; steamMachineAuth76561198832942031=00FA6438D4EFD3645FA96E4E8EBE9701B85EF355; browserid=1089217996877788449; recentlyVisitedAppHubs=730%2C225600%2C327070; app_impressions=730@2_9_100006_100202|730@2_9_100006_100202|225600@2_9_100006_100202|730@2_9_100006_100202|327070@2_9_100006_100202" -X POST https://steamcommunity.com/dev/deletegsaccount -o gslt/test.html'
+    os.system(command)
+    command = 'rm gslt/test.html'
+    os.system(command)
+    print('successfully deleted gslt for server with id -> test')
 
 
 creategslt()
 deletegslt()
+
 
 class Check(threading.Thread):
     def __init__(self):
