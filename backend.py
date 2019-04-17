26e9268a8f151292378beb5244e73923c5b99a04
Timo Aeckerle <timoaeckerle@gmx.de>
2019-04-17 18:29:28 +0200

    Add final implementation of gslt

--- backend.py
+++ backend.py
@@ -8,35 +8,6 @@ clients = []
 matches = []
 
 
-def creategslt():
-    print('try to create gslt for server with id -> test')
-    command = 'curl -d "appid=730&memo=' + 'test' + '&sessionid=7f6485556d7104ef41146653" --header "Cookie: sessionid=7f6485556d7104ef41146653; steamCountry=DE%7C1014e5d9cdff2826218d409fb1eb5a56; timezoneOffset=7200,0; _ga=GA1.2.275275590.1555411498; steamLoginSecure=76561198832942031%7C%7CD71E1EE78B42609277E0314337FD4546AD467576; steamMachineAuth76561198832942031=00FA6438D4EFD3645FA96E4E8EBE9701B85EF355; browserid=1089217996877788449; recentlyVisitedAppHubs=730%2C225600%2C327070; app_impressions=730@2_9_100006_100202|730@2_9_100006_100202|225600@2_9_100006_100202|730@2_9_100006_100202|327070@2_9_100006_100202" -X POST https://steamcommunity.com/dev/creategsaccount -o gslt/test.html'
-    os.system(command)
-    command = 'curl https://steamcommunity.com/dev/managegameservers --header "Cookie: sessionid=7f6485556d7104ef41146653; steamCountry=DE%7C1014e5d9cdff2826218d409fb1eb5a56; timezoneOffset=7200,0; _ga=GA1.2.275275590.1555411498; steamLoginSecure=76561198832942031%7C%7CD71E1EE78B42609277E0314337FD4546AD467576; steamMachineAuth76561198832942031=00FA6438D4EFD3645FA96E4E8EBE9701B85EF355; browserid=1089217996877788449; recentlyVisitedAppHubs=730%2C225600%2C327070; app_impressions=730@2_9_100006_100202|730@2_9_100006_100202|225600@2_9_100006_100202|730@2_9_100006_100202|327070@2_9_100006_100202" -o gslt/test.html'
-    os.system(command)
-    html = open("gslt/test.html").read()
-    soup = BeautifulSoup(html, 'html.parser')
-    gslt = soup.find("td", string="test").find_previous_sibling("td").find_previous_sibling("td").decode_contents()
-    print('gslt -> ' + gslt)
-    print('successfully created gslt for server with id -> test')
-
-
-def deletegslt():
-    print('try to delete gslt for server with id -> test')
-    html = open("gslt/test.html").read()
-    soup = BeautifulSoup(html, 'html.parser')
-    steamid = soup.find("td", string="test").find_next_sibling("td").find("input", {"name":"steamid"})['value']
-    command = 'curl -d "steamid=' + steamid + '&sessionid=7f6485556d7104ef41146653" --header "Cookie: sessionid=7f6485556d7104ef41146653; steamCountry=DE%7C1014e5d9cdff2826218d409fb1eb5a56; timezoneOffset=7200,0; _ga=GA1.2.275275590.1555411498; steamLoginSecure=76561198832942031%7C%7CD71E1EE78B42609277E0314337FD4546AD467576; steamMachineAuth76561198832942031=00FA6438D4EFD3645FA96E4E8EBE9701B85EF355; browserid=1089217996877788449; recentlyVisitedAppHubs=730%2C225600%2C327070; app_impressions=730@2_9_100006_100202|730@2_9_100006_100202|225600@2_9_100006_100202|730@2_9_100006_100202|327070@2_9_100006_100202" -X POST https://steamcommunity.com/dev/deletegsaccount -o gslt/test.html'
-    os.system(command)
-    command = 'rm gslt/test.html'
-    os.system(command)
-    print('successfully deleted gslt for server with id -> test')
-
-
-creategslt()
-deletegslt()
-
-
 class Check(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
@@ -56,10 +27,10 @@ class Check(threading.Thread):
 
 
 class Match(threading.Thread):
-    def __init__(self, name, code, port, mapid):
+    def __init__(self, name, gslt, port, mapid):
         threading.Thread.__init__(self)
         self.name = name
-        self.code = code
+        self.gslt = gslt
         self.port = port
         self.mapid = mapid
         self.canDelete = False
@@ -73,20 +44,33 @@ class Match(threading.Thread):
 
     def creategslt(self):
         print('try to create gslt for server with id -> ' + self.name)
-        command = 'curl -d "appid=730&memo=' + self.name + '&sessionid=7f6485556d7104ef41146653" --header "Host: steamcommunity.com" --header "Connection: keep-alive" --header "Cache-Control: max-age=0" --header "Origin: https://steamcommunity.com" --header "Upgrade-Insecure-Requests: 1" --header "Content-Type: application/x-www-form-urlencoded" --header "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3" --header "Referer: https://steamcommunity.com/dev/managegameservers" --header "Accept-Encoding: gzip, deflate, br" --header "Accept-Language: de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7" --header "Cookie: sessionid=7f6485556d7104ef41146653; steamCountry=DE%7C1014e5d9cdff2826218d409fb1eb5a56; timezoneOffset=7200,0; _ga=GA1.2.275275590.1555411498; steamLoginSecure=76561198832942031%7C%7CD71E1EE78B42609277E0314337FD4546AD467576; steamMachineAuth76561198832942031=00FA6438D4EFD3645FA96E4E8EBE9701B85EF355; browserid=1089217996877788449; recentlyVisitedAppHubs=730%2C225600%2C327070; app_impressions=730@2_9_100006_100202|730@2_9_100006_100202|225600@2_9_100006_100202|730@2_9_100006_100202|327070@2_9_100006_100202" -X POST https://steamcommunity.com/dev/creategsaccount -o test.html'
+        command = 'curl -d "appid=730&memo=' + self.name + '&sessionid=7f6485556d7104ef41146653" --header "Host: steamcommunity.com" --header "Connection: keep-alive" --header "Cache-Control: max-age=0" --header "Origin: https://steamcommunity.com" --header "Upgrade-Insecure-Requests: 1" --header "Content-Type: application/x-www-form-urlencoded" --header "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3" --header "Referer: https://steamcommunity.com/dev/managegameservers" --header "Accept-Encoding: gzip, deflate, br" --header "Accept-Language: de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7" --header "Cookie: sessionid=7f6485556d7104ef41146653; steamCountry=DE%7C1014e5d9cdff2826218d409fb1eb5a56; timezoneOffset=7200,0; _ga=GA1.2.275275590.1555411498; steamLoginSecure=76561198832942031%7C%7CD71E1EE78B42609277E0314337FD4546AD467576; steamMachineAuth76561198832942031=00FA6438D4EFD3645FA96E4E8EBE9701B85EF355; browserid=1089217996877788449; recentlyVisitedAppHubs=730%2C225600%2C327070; app_impressions=730@2_9_100006_100202|730@2_9_100006_100202|225600@2_9_100006_100202|730@2_9_100006_100202|327070@2_9_100006_100202" -X POST https://steamcommunity.com/dev/creategsaccount -o gslt/' + self.name + '.html'
         os.system(command)
+        command = 'curl https://steamcommunity.com/dev/managegameservers --header "Cookie: sessionid=7f6485556d7104ef41146653; steamCountry=DE%7C1014e5d9cdff2826218d409fb1eb5a56; timezoneOffset=7200,0; _ga=GA1.2.275275590.1555411498; steamLoginSecure=76561198832942031%7C%7CD71E1EE78B42609277E0314337FD4546AD467576; steamMachineAuth76561198832942031=00FA6438D4EFD3645FA96E4E8EBE9701B85EF355; browserid=1089217996877788449; recentlyVisitedAppHubs=730%2C225600%2C327070; app_impressions=730@2_9_100006_100202|730@2_9_100006_100202|225600@2_9_100006_100202|730@2_9_100006_100202|327070@2_9_100006_100202" -o gslt/' + self.name + '.html'
+        os.system(command)
+        html = open("gslt/" + self.name + '.html').read()
+        self.gslt = BeautifulSoup(html, 'html.parser').find("td", string="" + self.name).find_previous_sibling("td").find_previous_sibling("td").decode_contents()
+        print('successfully created gslt -> ' + self.gslt + ' for server with id -> ' + self.name)
 
     def deletegslt(self):
-        pass
+        print('try to delete gslt -> ' + self.gslt + ' for server with id -> ' + self.name)
+        html = open("gslt/" + self.name + '.html').read()
+        steamid = BeautifulSoup(html, 'html.parser').find("td", string="" + self.name).find_next_sibling("td").find("input", {"name":"steamid"})['value']
+        command = 'curl -d "steamid=' + steamid + '&sessionid=7f6485556d7104ef41146653" --header "Cookie: sessionid=7f6485556d7104ef41146653; steamCountry=DE%7C1014e5d9cdff2826218d409fb1eb5a56; timezoneOffset=7200,0; _ga=GA1.2.275275590.1555411498; steamLoginSecure=76561198832942031%7C%7CD71E1EE78B42609277E0314337FD4546AD467576; steamMachineAuth76561198832942031=00FA6438D4EFD3645FA96E4E8EBE9701B85EF355; browserid=1089217996877788449; recentlyVisitedAppHubs=730%2C225600%2C327070; app_impressions=730@2_9_100006_100202|730@2_9_100006_100202|225600@2_9_100006_100202|730@2_9_100006_100202|327070@2_9_100006_100202" -X POST https://steamcommunity.com/dev/deletegsaccount -o gslt/' + self.name + '.html'
+        os.system(command)
+        command = 'rm gslt/' + self.name + '.html'
+        os.system(command)
+        print('successfully deleted gslt -> ' + self.gslt + ' for server with id -> ' + self.name)
 
     def create(self):
         print('try to create and start the server')
+        self.creategslt()
         command = '/home/mastermind/csgo-multiserver/csgo-server @' + self.name + ' create'
         os.system(command)
         with open('/root/csgo@' + self.name + '/msm.d/cfg/server.conf', 'r') as f:
             s = f.read()
         with open('/root/csgo@' + self.name + '/msm.d/cfg/server.conf', 'w') as f:
-            s = s.replace('${GSLT-""}', '${GSLT-"' + self.code + '"}')
+            s = s.replace('${GSLT-""}', '${GSLT-"' + self.gslt + '"}')
             s = s.replace('${PORT-"27015"}', '${PORT-"' + self.port + '"}')
             s = s.replace('+mapgroup $MAPGROUP', '-authkey A81E42AF2DDFDC28A9B13CE43901F112')
             s = s.replace('+map $MAP', '+host_workshop_map ' + self.mapid)
@@ -97,6 +81,7 @@ class Match(threading.Thread):
 
     def delete(self):
         print('try to stop and delete the server')
+        self.deletegslt()
         command = '/home/mastermind/csgo-multiserver/csgo-server @' + self.name + ' stop'
         os.system(command)
         command = 'rm -r /root/csgo@' + self.name
@@ -111,7 +96,7 @@ class Backend(WebSocket):
         print('message received from: ' + self.address[0] + " -> " + self.data)
         data = self.data.split(" ")
         print(data)
-        if data[0] == 'c':  # name -> data[1] | code -> data[2] | port -> data[3] | mapid -> data[4]
+        if data[0] == 'c':  # name -> data[1] | gslt -> data[2] | port -> data[3] | mapid -> data[4]
             match = Match(data[1], data[2], data[3], data[4])
             match.start()
             matches.append(match)
